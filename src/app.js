89f888543a2775ab2671c8249e5b69c2bd8c7af0
Bruno de Queirós <42852056+bqdesigner@users.noreply.github.com>
2019-09-05 22:56:51 -0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+import React from 'react'
+import Title from './title'
+
+var App = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <Title name='Talitinha' saudacao='tudo bem?' />
+      </div>
+    )
+  }
+})
+
+export default App
